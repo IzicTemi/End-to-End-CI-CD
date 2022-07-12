14b9f80e07378717dd644fc2351f695360d2ab75
IzicTemi <iomolayo2541@gmail.com>
2022-07-12 20:21:53 +0100

    push

---
+++ bill.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+OldWorkflowID=$( cat ../urls.txt )
+echo $OldWorkflowID
+STACKS=$($(aws cloudformation list-stacks --query "StackSummaries[*].StackName" --stack-status-filter CREATE_COMPLETE --no-paginate --output text)) 
+echo Stack names: "${STACKS}"
